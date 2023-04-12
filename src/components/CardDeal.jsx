586d9ebab356d7b8262cea24778b312c4a83ffbc
emanikor <87859876+emanikor@users.noreply.github.com>
2023-04-12 15:41:59 +0300

    Add files via upload

---
+++ src/components/CardDeal.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CardDeal = () => {
+  return (
+    <div>cardDeal</div>
+  )
+}
+
+export default CardDeal
---
+++ src/components/Stats.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Stats = () => {
+  return (
+    <div>stats</div>
+  )
+}
+
+export default Stats
